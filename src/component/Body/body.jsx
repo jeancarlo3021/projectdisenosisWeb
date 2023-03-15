0aeffbd9267a2fa7614a8bde92836849ef285334
jeancarloom12@gmail.com <jeancarloom12@gmail.com>
2023-03-14 18:12:45 -0600

    delete Filtro and fixed body

--- src/component/Body/body.jsx
+++ src/component/Body/body.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import 'bootstrap'
-import Filtro from './Filtro'
 import Producto from './productos'
 import Names from './names'
 import { Link } from 'react-router-dom'
@@ -8,7 +7,6 @@ import { Link } from 'react-router-dom'
 const Body = () => {
   return (
     <>
-      <Filtro />
       <Names />
       <Producto />
       <Link to={'/login'}>a</Link>
